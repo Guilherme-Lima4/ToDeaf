bc9d0fc96aca9246e58975be4b8fc3926c91437b
guilhermebis <guilherme.lima.dinizt@gmail.com>
2019-10-30 20:44:28 -0300

    Projeto reiniciado

--- App.js
+++ App.js
@@ -5,43 +5,110 @@
  * @format
  * @flow
  */
+
 import React from 'react';
-import { View, TextInput, Button } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
-import { withFormik } from 'formik';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-const CadastrarInst = (props) => (
-  <View style={styles.container}>
-    <TextInput
-      value={props.values.NomeInst}
-      onChangeText={text => props.setFieldValue('Nome da Instituição', text)}
-    />
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
 
-    <TextInput
-      value={props.values.CPF_CNPJ}
-      onChangeText={text => props.setFieldValue('CPF/CNPJ', text)}
-    />
-    <TextInput
-      value={props.values.End_Rua}
-      onChangeText={text => props.setFieldValue('Rua', text)}
-    />
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
 
-    <TextInput
-      value={props.values.End_Num}
-      onChangeText={text => props.setFieldValue('N°', text)}
-    />
-
-    <Button
-      onPress={props.handleSubmit}
-      title="Cadastrar"
-    />
-  </View>
-);
-
-export default withFormik({
-  mapPropsToValues: () => ({ NomeInst: '', CPF_CNPJ: '',  End_Rua: '', End_Num: '' }),
-
-  handleSubmit: (values) => {
-    console.log(values);
-  }
-})(CadastrarIsnt);
+export default App;
